78d7d531ac277f552196ca248ee9aeb42fb2a1b9
Daniel Johansson <daniel.johansson@wireweb.co.uk>
2015-04-22 16:44:52 +0100

    Added player stats file and gitignore.

---
+++ player-stats.js
@@ -0,0 +1,183 @@
+var players = [
+    {
+        name: "Daniel Johansson",
+        type: "ROSTERED",
+        role: "CAPTAIN",
+        stats: {
+            atBat: 3,
+            hits: 2,
+            walks: 0,
+            runsBattedIn: 2,
+            firstBase: 1,
+            secondBase: 0,
+            thirdBase: 0,
+            homeRuns: 1,
+            struckOut: 1
+        }
+    },
+    {
+        name: "Darren Corner",
+        type: "ROSTERED",
+        stats: {
+            atBat: 4,
+            hits: 3,
+            walks: 1,
+            runsBattedIn: 3,
+            firstBase: 1,
+            secondBase: 0,
+            thirdBase: 0,
+            homeRuns: 2,
+            struckOut: 0
+        }
+    },
+    {
+        name: "Ian Mills",
+        type: "ROSTERED",
+        stats: {
+            atBat: 4,
+            hits: 4,
+            walks: 0,
+            runsBattedIn: 5,
+            firstBase: 1,
+            secondBase: 1,
+            thirdBase: 0,
+            homeRuns: 2,
+            struckOut: 0
+        }
+    },
+    {
+        name: "Andy Shaw",
+        type: "ROSTERED",
+        stats: {
+            atBat: 3,
+            hits: 3,
+            walks: 0,
+            runsBattedIn: 1,
+            firstBase: 3,
+            secondBase: 0,
+            thirdBase: 0,
+            homeRuns: 0,
+            struckOut: 0
+        }
+    },
+    {
+        name: "Dave Ogden",
+        type: "ROSTERED",
+        stats: {
+            atBat: 4,
+            hits: 2,
+            walks: 0,
+            runsBattedIn: 2,
+            firstBase: 1,
+            secondBase: 1,
+            thirdBase: 0,
+            homeRuns: 0,
+            struckOut: 0
+        }
+    },
+    {
+        name: "Ian Radford",
+        type: "ROSTERED",
+        stats: {
+            atBat: 4,
+            hits: 4,
+            walks: 0,
+            runsBattedIn: 2,
+            firstBase: 4,
+            secondBase: 0,
+            thirdBase: 0,
+            homeRuns: 0,
+            struckOut: 0
+        }
+    },
+    {
+        name: "Amy Calladine",
+        type: "ROSTERED",
+        stats: {
+            atBat: 4,
+            hits: 3,
+            walks: 0,
+            runsBattedIn: 0,
+            firstBase: 3,
+            secondBase: 0,
+            thirdBase: 0,
+            homeRuns: 0,
+            struckOut: 1
+        }
+    },
+    {
+        name: "Bev Hurren",
+        type: "ROSTERED",
+        stats: {
+            atBat: 4,
+            hits: 2,
+            walks: 0,
+            runsBattedIn: 1,
+            firstBase: 2,
+            secondBase: 0,
+            thirdBase: 0,
+            homeRuns: 0,
+            struckOut: 0
+        }
+    },
+    {
+        name: "Ella Mills",
+        type: "ROSTERED",
+        stats: {
+            atBat: 4,
+            hits: 1,
+            walks: 3,
+            runsBattedIn: 0,
+            firstBase: 1,
+            secondBase: 0,
+            thirdBase: 0,
+            homeRuns: 0,
+            struckOut: 0
+        }
+    },
+    {
+        name: "Kay Carter",
+        type: "ROSTERED",
+        stats: {
+            atBat: 3,
+            hits: 2,
+            walks: 0,
+            runsBattedIn: 0,
+            firstBase: 1,
+            secondBase: 1,
+            thirdBase: 0,
+            homeRuns: 0,
+            struckOut: 1
+        }
+    },
+    {
+        name: "Suzi Wolfram",
+        type: "ROSTERED",
+        stats: {
+            atBat: 4,
+            hits: 4,
+            walks: 0,
+            runsBattedIn: 0,
+            firstBase: 1,
+            secondBase: 1,
+            thirdBase: 0,
+            homeRuns: 0,
+            struckOut: 1
+        }
+    },
+    {
+        name: "Jane Curley",
+        type: "GUEST",
+        stats: {
+            atBat: 3,
+            hits: 1,
+            walks: 0,
+            runsBattedIn: 2,
+            firstBase: 1,
+            secondBase: 0,
+            thirdBase: 0,
+            homeRuns: 0,
+            struckOut: 0
+        }
+    }
+];
