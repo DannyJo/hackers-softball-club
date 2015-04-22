fab25ed8976386799b6fce5d86fe95a66e8c1731
Daniel Johansson <daniel.johansson@wireweb.co.uk>
2015-04-22 20:26:31 +0100

    Added match list and league table.

---
+++ matches.js
@@ -0,0 +1,131 @@
+// Outcome must be one of "WIN", "LOSS" or "FIXTURE"
+
+var matches = [
+    {
+        outcome: "WIN",
+        place: "Home",
+        date: "21/04/2015",
+        opposition: "Drizzle",
+        result: "8 - 23",
+        mvp: "Ella Mills & Ian Mills"
+    },
+    {
+        outcome: "FIXTURE",
+        place: "Away",
+        date: "28/04/2015",
+        opposition: "Barflies",
+        result: ""
+    },
+    {
+        outcome: "FIXTURE",
+        place: "Home",
+        date: "05/05/2015",
+        opposition: "Freeze",
+        result: ""
+    },
+    {
+        outcome: "RESTING",
+        place: "",
+        date: "12/05/2015",
+        opposition: "",
+        result: ""
+    },
+    {
+        outcome: "FIXTURE",
+        place: "Away",
+        date: "19/05/2015",
+        opposition: "Phoenix",
+        result: ""
+    },
+    {
+        outcome: "FIXTURE",
+        place: "Home",
+        date: "26/05/2015",
+        opposition: "Moorkats",
+        result: ""
+    },
+    {
+        outcome: "FIXTURE",
+        place: "Home",
+        date: "02/06/2015",
+        opposition: "Wildcats",
+        result: ""
+    },
+    {
+        outcome: "FIXTURE",
+        place: "Away",
+        date: "09/06/2015",
+        opposition: "Colt 45s",
+        result: ""
+    },
+    {
+        outcome: "FIXTURE",
+        place: "Home",
+        date: "16/06/2015",
+        opposition: "Mavericks II",
+        result: ""
+    },
+    {
+        outcome: "FIXTURE",
+        place: "Away",
+        date: "23/06/2015",
+        opposition: "Drizzle",
+        result: ""
+    },
+    {
+        outcome: "FIXTURE",
+        place: "Home",
+        date: "30/06/2015",
+        opposition: "Barflies",
+        result: ""
+    },
+    {
+        outcome: "FIXTURE",
+        place: "Away",
+        date: "07/07/2015",
+        opposition: "Freeze",
+        result: ""
+    },
+    {
+        outcome: "RESTING",
+        place: "",
+        date: "14/07/2015",
+        opposition: "",
+        result: ""
+    },
+    {
+        outcome: "FIXTURE",
+        place: "Home",
+        date: "21/07/2015",
+        opposition: "Phoenix",
+        result: ""
+    },
+    {
+        outcome: "FIXTURE",
+        place: "Away",
+        date: "28/07/2015",
+        opposition: "Moorkats",
+        result: ""
+    },
+    {
+        outcome: "FIXTURE",
+        place: "Away",
+        date: "04/08/2015",
+        opposition: "Wildcats",
+        result: ""
+    },
+    {
+        outcome: "FIXTURE",
+        place: "Home",
+        date: "11/08/2015",
+        opposition: "Colt 45s",
+        result: ""
+    },
+    {
+        outcome: "FIXTURE",
+        place: "Away",
+        date: "18/08/2015",
+        opposition: "Mavericks II",
+        result: ""
+    }
+];
