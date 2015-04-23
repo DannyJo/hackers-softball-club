504000a896206f49b9ef0039cb3379a6fc9a3eee
Daniel Johansson <daniel.johansson@wireweb.co.uk>
2015-04-23 09:35:32 +0100

    Added pitch location and pitch map.

--- matches.js
+++ matches.js
@@ -7,125 +7,143 @@ var matches = [
         date: "21/04/2015",
         opposition: "Drizzle",
         result: "8 - 23",
-        mvp: "Ella Mills & Ian Mills"
+        mvp: "Ella Mills & Ian Mills",
+        pitch: "PW 8"
     },
     {
         outcome: "FIXTURE",
         place: "Away",
         date: "28/04/2015",
         opposition: "Barflies",
-        result: ""
+        result: "",
+        pitch: "PW 2"
     },
     {
         outcome: "FIXTURE",
         place: "Home",
         date: "05/05/2015",
         opposition: "Freeze",
-        result: ""
+        result: "",
+        pitch: "PW 7"
     },
     {
         outcome: "RESTING",
         place: "",
         date: "12/05/2015",
         opposition: "",
-        result: ""
+        result: "",
+        pitch: ""
     },
     {
         outcome: "FIXTURE",
         place: "Away",
         date: "19/05/2015",
         opposition: "Phoenix",
-        result: ""
+        result: "",
+        pitch: "PW 9"
     },
     {
         outcome: "FIXTURE",
         place: "Home",
         date: "26/05/2015",
         opposition: "Moorkats",
-        result: ""
+        result: "",
+        pitch: "PW 1"
     },
     {
         outcome: "FIXTURE",
         place: "Home",
         date: "02/06/2015",
         opposition: "Wildcats",
-        result: ""
+        result: "",
+        pitch: "PW 3"
     },
     {
         outcome: "FIXTURE",
         place: "Away",
         date: "09/06/2015",
         opposition: "Colt 45s",
-        result: ""
+        result: "",
+        pitch: "TBA"
     },
     {
         outcome: "FIXTURE",
         place: "Home",
         date: "16/06/2015",
         opposition: "Mavericks II",
-        result: ""
+        result: "",
+        pitch: "TBA"
     },
     {
         outcome: "FIXTURE",
         place: "Away",
         date: "23/06/2015",
         opposition: "Drizzle",
-        result: ""
+        result: "",
+        pitch: "TBA"
     },
     {
         outcome: "FIXTURE",
         place: "Home",
         date: "30/06/2015",
         opposition: "Barflies",
-        result: ""
+        result: "",
+        pitch: "TBA"
     },
     {
         outcome: "FIXTURE",
         place: "Away",
         date: "07/07/2015",
         opposition: "Freeze",
-        result: ""
+        result: "",
+        pitch: "TBA"
     },
     {
         outcome: "RESTING",
         place: "",
         date: "14/07/2015",
         opposition: "",
-        result: ""
+        result: "",
+        pitch: ""
     },
     {
         outcome: "FIXTURE",
         place: "Home",
         date: "21/07/2015",
         opposition: "Phoenix",
-        result: ""
+        result: "",
+        pitch: "TBA"
     },
     {
         outcome: "FIXTURE",
         place: "Away",
         date: "28/07/2015",
         opposition: "Moorkats",
-        result: ""
+        result: "",
+        pitch: "TBA"
     },
     {
         outcome: "FIXTURE",
         place: "Away",
         date: "04/08/2015",
         opposition: "Wildcats",
-        result: ""
+        result: "",
+        pitch: "TBA"
     },
     {
         outcome: "FIXTURE",
         place: "Home",
         date: "11/08/2015",
         opposition: "Colt 45s",
-        result: ""
+        result: "",
+        pitch: "TBA"
     },
     {
         outcome: "FIXTURE",
         place: "Away",
         date: "18/08/2015",
         opposition: "Mavericks II",
-        result: ""
+        result: "",
+        pitch: "TBA"
     }
 ];
